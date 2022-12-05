126feee79a3f9078aa8b11541b84c06e85315e07
brunogroth <brunom.groth@gmail.com>
2022-12-04 23:23:54 -0300

    feat: Async methods Get Products and Get By Id

--- API/Controllers/ProductsController.cs
+++ API/Controllers/ProductsController.cs
@@ -2,20 +2,32 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using API.Data;
+using API.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace API.Controllers;
 
 [ApiController]
 [Route("api/[controller]")]
 public class ProductsController : ControllerBase{
+    public readonly StoreContext _context;
+    public ProductsController(StoreContext context)
+    {
+      _context = context;
+
+    }
+
     [HttpGet]
-    public string GetProducts(){
-        return "this will be a list of products";
+    public async Task<ActionResult<List<Product>>> GetProducts(){
+        var products = await _context.Products.ToListAsync();
+
+        return Ok(products);
     }
 
     [HttpGet("{id}")]
-    public string GetProductById(int id){
-        return "oneProduct";
+    public async Task<ActionResult<Product>> GetProductById(int id){
+        return await _context.Products.FindAsync(id);
     }
 }
